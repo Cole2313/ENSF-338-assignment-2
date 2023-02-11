ca2d194b112b66f23e0f172714e36bf09dd88347
Cole2313 <113648243+Cole2313@users.noreply.github.com>
2023-02-10 20:33:50 -0700

    added the code into files 1.3 and 1.5

--- ex1.3.py
+++ ex1.3.py
@@ -1,1 +1,9 @@
+def func(n, fibnum = {}):
+    if n == 0 or n == 1:
+        return n
+    for num in fibnum:
+        if num == n:
+            return fibnum[n]
+    fibnum[n] = func(n-1) + func(n-2)
+    return fibnum[n]
 
--- ex1.5.py
+++ ex1.5.py
@@ -1,1 +1,15 @@
 
+
+times = []
+indexs = []
+for i in range(36):
+    times.append(timeit.timeit(lambda: func(i), number = 1))
+    indexs.append(i)
+print(times)
+print(indexs)
+
+plt.title("Time to calculate numbers 0-35 in the fibonacci sequence")
+plt.plot(indexs, times)
+plt.xlabel("fibonacci index")
+plt.ylabel("Time (s)")
+plt.show()
